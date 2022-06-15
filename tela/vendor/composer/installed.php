0add1e530b0b82e3bd266787b5f2451e040c5108
Elisa Ciancarelli <elisa.ciancarelli@condexo.it>
2022-06-16 00:53:25 +0200

    congfigurato php

---
+++ tela/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '330fcb5e9122a3bf50dd0607496e1e0df37da30b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '330fcb5e9122a3bf50dd0607496e1e0df37da30b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
