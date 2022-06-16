80a3d18acf4ee2df2b459c04ad8445b9fb4eb9b1
Elisa Ciancarelli <elisa.ciancarelli@condexo.it>
2022-06-16 22:30:39 +0200

    fix composer

--- tela/pages/_app.tsx
+++ tela/pages/_app.tsx
@@ -2,10 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { store } from "../store";
-import { createMemoryHistory } from 'history';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const history = createMemoryHistory();
 
   return (
     <Provider store={store}>
