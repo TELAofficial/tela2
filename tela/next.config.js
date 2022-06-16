90f0124ffc21eceed59e715cbd2faa997b4c1759
Elisa Ciancarelli <elisa.ciancarelli@condexo.it>
2022-06-16 23:16:16 +0200

    add configuration

--- tela/next.config.js
+++ tela/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+    // ... rest of the configuration.
+    experimental: {
+        outputStandalone: true,
+    },
+}
